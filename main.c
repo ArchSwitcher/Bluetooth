fe5fae7b4535f829686e1aba8d0ebac3e7be53b8
elpablo92 <zapeta.pablo@gmail.com>
2020-02-28 21:08:45 -0600

    primer bt

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: dell
+ *
+ * Created on February 28, 2020, 9:06 PM
+ */
+
+
+#include <xc.h>
+#include"config.h"
+#include"USART_Header_File.h"
+
+void main(void) {
+    return;
+}
